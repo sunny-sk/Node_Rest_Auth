e41174cd50740500ccf160f92cca7271663bfe9d
sunny-sk <sk956223@gmail.com>
2020-03-05 08:58:43 +0530

    restructured changes

--- index.js
+++ index.js
@@ -1,83 +1,28 @@
 const express = require("express");
 const app = express();
-const color = require("colors");
+require("colors");
 require("dotenv").config();
 const morgan = require("morgan");
-const Joi = require("@hapi/joi");
 
-app.use(morgan("dev"));
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("in development mode");
+}
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
-const courses = [
-  {
-    id: 1,
-    name: "course1"
-  },
-  {
-    id: 2,
-    name: "course1"
-  },
-  {
-    id: 3,
-    name: "course1"
-  }
-];
-
-app.get("/api/courses", (req, res, next) => {
-  res.send(courses);
-});
-
-app.get("/api/courses/:id", (req, res, next) => {
-  const course = courses.find(course => course.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("not found");
-
-  res.send(course);
-});
-
-app.post("/api/courses", (req, res, next) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res, next) => {
-  const course = courses.find(course => course.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("not found");
-
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details);
-
-  course.name = req.body.name;
-  res.status(200).send(course);
-});
-
-app.delete("/api/courses/:id", (req, res, next) => {
-  console.log(typeof req.params.id);
-  const course = courses.find(course => course.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("not found");
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.status(200).send(courses);
-});
+const courses = require("./routes/genres");
+app.use("/api/genres/", courses);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started at ${process.env.PORT}`);
 });
-
-function validateCourse(course) {
-  const scheme = Joi.object({
-    name: Joi.string()
-      .min(5)
-      .required()
-  });
-  return scheme.validate(course);
-}
---
+++ routes/genres.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("@hapi/joi");
+
+const genres = [
+  {
+    id: 1,
+    name: "course1"
+  },
+  {
+    id: 2,
+    name: "course1"
+  },
+  {
+    id: 3,
+    name: "course1"
+  }
+];
+
+router.get("/", (req, res, next) => {
+  res.send(genres);
+});
+
+router.get("/:id", (req, res, next) => {
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("not found");
+  res.send(genre);
+});
+
+router.post("/", (req, res, next) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+router.put("/:id", (req, res, next) => {
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("not found");
+
+  const { error } = validategenre(req.body);
+  if (error) return res.status(400).send(error.details);
+
+  genre.name = req.body.name;
+  res.status(200).send(genre);
+});
+
+router.delete("/:id", (req, res, next) => {
+  console.log(typeof req.params.id);
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("not found");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.status(200).send(genres);
+});
+
+module.exports = router;
+
+function validateGenre(genre) {
+  const scheme = Joi.object({
+    name: Joi.string()
+      .min(5)
+      .required()
+  });
+  return scheme.validate(genre);
+}
